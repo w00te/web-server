d727fc482c762c1a212a30e27c84430b4da7c218
John Humphreys <johnwilliamhumphreys@gmail.com>
2016-06-26 21:51:45 -0400

    John Humphreys - Checking in middleware refactoring.

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
+var middleware = require('./middleware');
+
 var app = express();
 var PORT = 3000;
-
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	} 
-};
 
 app.use(middleware.logger);
 
